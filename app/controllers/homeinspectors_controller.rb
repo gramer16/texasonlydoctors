3fa0d2c9aeb3756ce552c2fe20925a93cefe4daa
graciem <gramer16@dnginternational.ca>
2015-07-19 16:18:30 -0600

    F

--- app/controllers/homeinspectors_controller.rb
+++ app/controllers/homeinspectors_controller.rb
@@ -1,5 +1,6 @@
 class HomeinspectorsController < ApplicationController
   before_action :set_homeinspector, only: [:show, :edit, :update, :destroy]
+  before_action :check_user, only: [:destroy, :edit] 
    def search
     if params[:search].present?
       @homeinspectors = Homeinspector.search(params[:search])
@@ -64,4 +65,9 @@ end
     def homeinspector_params
       params.require(:homeinspector).permit(:company_name, :company_description, :address, :city, :zipcode, :contact_name, :company_website, :company_phone, :image)
     end
+     def check_user
+        unless current_user.admin?
+         redirect_to root_url, alert: "Sorry, Only Ontario's Only Admin can Delete a Subscription"
+    end
+  end
 end
